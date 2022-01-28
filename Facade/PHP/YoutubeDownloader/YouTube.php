5f585b915b0620038fa04c7b6462754161e54278
chun-hunag <hgfrttlk@gmail.com>
2022-01-28 21:52:20 +0800

    [Facade] Add PHP example - ConceptSample, YoutuberDownloader

---
+++ Facade/PHP/YoutubeDownloader/YouTube.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * The YouTube API subsystem.
+ */
+class YouTube
+{
+    public function fetchVideo(): string { /* ... */ }
+
+    public function saveAs(string $path): void { /* ... */ }
+
+    // ...more methods and classes...
+}
