872957071a8c286393a157acc8691cca50d2f30a
chun-huang <hgfrttlk@gmail.com>
2022-04-12 23:18:50 +0800

    [Singleton] Add GoLang example

---
+++ Singleton/GoLang/Singleton/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	db := GetSingletonDatabase()
+	pop := db.GetPopulation("Seoul")
+	fmt.Println("Pop of Seoul = ", pop)
+
+	cities := []string{"Seoul", "Mexico City"}
+	//tp := GetTotalPopulation(cities)
+	tp := GetTotalPopulationEx(GetSingletonDatabase(), cities)
+
+	ok := tp == (17500000 + 17400000) // testing on live data
+	fmt.Println(ok)
+
+	names := []string{"alpha", "gamma"} // expect 4
+	tp = GetTotalPopulationEx(&DummyDatabase{}, names)
+	ok = tp == 4
+	fmt.Println(ok)
